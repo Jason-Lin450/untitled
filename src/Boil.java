ed7b29f1ed7bcb1f0db6ec865eb54ec7cf4dd972
BT_1N3_09 <sample@bths.edu>
2025-11-21 10:18:11 -0500

    boil better bro

---
+++ src/Boil.java
@@ -0,0 +1,6 @@
+public class Boil {
+    public static void main(String[] args) {
+        System.out.println("boil better bro");
+        System.out.println("I hate coil");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("defg");
+        System.out.println("another chang3e");
         System.out.println("changed");
         int boil = 67;
         System.out.println(boil);
-        while (boil < 670) {
+        while (boil < 500) {
             boil++;
             System.out.println(boil);
         }
-        if (boil == 670) {
+        if (boil == 1) {
             System.out.println(boil - 98347 + " is better than coil");
         }
     }

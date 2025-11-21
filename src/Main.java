f667f185985375289615d5dfad9f475c437f0b29
BT_1N3_09 <sample@bths.edu>
2025-11-21 10:12:05 -0500

    added boil

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("abcdefg");
+        System.out.println("defg");
+        System.out.println("changee");
+        int boil = 67;
+        System.out.println(boil);
+        while (true) {
+            boil++;
+            System.out.println(boil);
+        }
     }
 }

ca3e6c42188e8718d10624cc92b6d13f7385c130
BT_1N3_09 <sample@bths.edu>
2025-11-21 10:14:01 -0500

    better than coil

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("defg");
-        System.out.println("changee");
+        System.out.println("changed");
         int boil = 67;
         System.out.println(boil);
-        while (true) {
+        while (boil < 670) {
             boil++;
             System.out.println(boil);
+        }
+        if (boil == 670) {
+            System.out.println(boil - 98347 + " is better than coil");
         }
     }
 }

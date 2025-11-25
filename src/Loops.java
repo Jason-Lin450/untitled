b3b0baf796a3b8864f92e681f49c141d9785c3d9
BT_1N3_09 <sample@bths.edu>
2025-11-25 10:13:56 -0500

    loops boil

---
+++ src/Loops.java
@@ -0,0 +1,27 @@
+public class Loops {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 5) {
+            System.out.println(counter);
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+        counter = 0;
+        while (counter < 5) {
+            counter++;
+            System.out.println(counter);
+        }
+        System.out.println("final counter: " + counter);
+        counter = 10;
+        while (counter < 5) {
+            System.out.println(counter);
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+        counter = 0;
+        while (counter >= 0) {
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+    }
+}

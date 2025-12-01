58a722cdc5cee1f140c19c2c2aee42ba3283324c
BT_1N3_09 <sample@bths.edu>
2025-12-01 11:04:19 -0500

    ad

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("another chang3e");
-        System.out.println("changed");
-        int boil = 67;
-        System.out.println(boil);
-        while (boil < 500) {
-            boil++;
-            System.out.println(boil);
+        ArrayList<String> lines = getFileData("src/data");
+        int num = 50;
+        int pass = 0;
+        for (String line : lines) {
+            if (line.substring(0,1).equals("L")) {
+                num -= Integer.parseInt(line.substring(1));
+                while (num < 0) {
+                    num += 100;
+                    pass++;
+                }
+            } else {
+                num += Integer.parseInt(line.substring(1));
+                while (num > 99) {
+                    num -= 100;
+                    pass++;
+                }
+            }
+            if (num == 0) {
+                pass++;
+            }
         }
-        while (boil > 1) {
-            boil--;
-            System.out.println(boil);
+        System.out.println(pass);
+    }
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
         }
-        if (boil == 1) {
-            System.out.println(boil + " is better than coil");
+        catch (FileNotFoundException e) {
+            return fileData;
         }
     }
 }
